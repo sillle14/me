45fa59799ad9fa1c860a8593fea52adeea0dd523
Lewis Silletto <lsilletto@caredash.com>
2021-04-03 13:10:53 -0400

    use hash router to avoid issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react'
-import { Router, RouteComponentProps, Link } from '@reach/router'
+import { BrowserRouter, Routes,  Link, HashRouter } from 'react-router-dom'
 
 
-let Home = (props: RouteComponentProps) => (
+let Home = (props: any) => (
   <div>
     <h1>Home</h1>
     <nav>
-      <Link to="/">Home</Link> |{" "}
-      <Link to="dashboard">Dashboard</Link>
+      <Link to={'/'}>Home</Link> |{" "}
+      <Link to={'dashboard'}>Dashboard</Link>
     </nav>
   </div>
 )
 
-let Dash = (props: RouteComponentProps) => <div>Dash</div>
+let Dash = (props: any) => <div>Dash</div>
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Home path="/" />
-        <Dash path="dashboard" />
-      </Router>
+      <HashRouter>
+      <Routes>
+        <Home path={'/'} />
+        <Dash path={'dashboard'} />
+      </Routes>
+      </HashRouter>
     </div>
   );
 }
