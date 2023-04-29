63f2a51e01a77d7fcf281dbce6b17e612ad1c9e6
Lewis Silletto <sillle14@gmail.com>
2023-04-29 12:11:34 -0400

    reorg

--- src/components/pages/roadTrip.tsx
+++ src/components/pages/roadTrip.tsx
@@ -2,42 +2,20 @@ import { Route, Routes } from "react-router";
 import Page from "../helpers/page";
 import Main from "../roadTrip/main";
 import Entry from "../roadTrip/entry";
-import odometer from '../../assets/road-trip/odometer.jpeg'
+import posts from "../roadTrip/posts";
 import { Link, Typography } from "@mui/material";
 import { Link as RouterLink } from 'react-router-dom'
 
-const posts = [
-    {
-        date: 'May 29th',
-        day: 1,
-        location: 'Boston, MA - Akron, OH',
-        drivingMiles: 100,
-        hikingMiles: 10,
-        content: [
-            {
-                type: 'p' as 'p',
-                text: 'This is the first paragraph. On this first day of driving, we did a bunch of things. Also, francesca is the ' +
-                'best cat ever and we miss her already. Also, there are some more things to say. And we also want to keep going with this ' +
-                'blog and yaddy yaddy yah and etc etc. Is this enough to break the line?'
-            },
-            {
-                type: 'img' as 'img',
-                alt: 'cat',
-                source: odometer,
-                caption: 'This is Francesca'
-            },
-            {
-                type: 'p' as 'p',
-                text: 'This is the next paragraph'
-            },
-        ]
-    }
-]
-
 const RoadTrip = () => {
-    const postComps = posts.map(
-        (post, i) => <Route key={i} path={`day-${post.day}`} element={<Entry entry={post}/>}></Route>
-    )
+    const postComps = posts.map((post, i) => {
+        const previous = i > 0 ? `day-${posts[i - 1].day}` : undefined
+        const next = i < posts.length - 1 ? `day-${posts[i + 1].day}` : undefined
+        return <Route 
+            key={i} 
+            path={`day-${post.day}`} 
+            element={<Entry entry={post} next={next} previous={previous}/>}
+        ></Route>
+    })
 
     return <Page maxWidth="lg">
         <Link to="" component={RouterLink} underline="hover" color='inherit'>
--- src/components/roadTrip/main.tsx
+++ src/components/roadTrip/main.tsx
@@ -10,26 +10,27 @@ const StyledImage = styled('img')(({theme}) => ({
 const Main = () => <>
     <StyledImage src={odometer} alt="starting odometer" />
     <Typography sx={{mb: 1}}>We're driving across the country! Follow along on our adventures.</Typography>
+    <hr></hr>
     <Grid container rowSpacing={1}>
-        <Grid item xs={6} sm={4}>
+        <Grid item xs={12} sm={4}>
             <Typography variant="h5">Heading West</Typography>
             <Link to="day-1" component={RouterLink} underline="hover">May 29th - Day 1</Link>
         </Grid>
-        <Grid item xs={6} sm={4}>
+        {/* <Grid item xs={12} sm={4}>
             <Typography variant="h5">Rocky Mountain National Park</Typography>
         </Grid>
-        <Grid item xs={6} sm={4}>
+        <Grid item xs={12} sm={4}>
             <Typography variant="h5">Yellowstone and the Tetons</Typography>
         </Grid>
-        <Grid item xs={6} sm={4}>
+        <Grid item xs={12} sm={4}>
             <Typography variant="h5">Rainier and Olympic</Typography>
         </Grid>
-        <Grid item xs={6} sm={4}>
+        <Grid item xs={12} sm={4}>
             <Typography variant="h5">Glacier</Typography>
         </Grid>
-        <Grid item xs={6} sm={4}>
+        <Grid item xs={12} sm={4}>
             <Typography variant="h5">Heading Back East</Typography>
-        </Grid>
+        </Grid> */}
     </Grid>
 </>
 
